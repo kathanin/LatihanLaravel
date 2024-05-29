fa0326b75090fb3f0f80cad71b1182d09ac25773
kathanin <aindra696@gmail.com>
2024-05-29 14:45:42 +0700

    Update Route

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\View\View;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Displays a listing of the resource.
+     * 
+     * @return View
+     */
+    public function index(): View
+    {
+        // Kirim data post ke view
+        return view('posts.index');
+    }
+}
